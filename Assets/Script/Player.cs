e34f38197be528faf81122e369cdae3f32e94b4c
txh1005 <txh1005@gmail.com>
2023-03-04 13:24:00 +0700

    test

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    public float speed;
+    private Vector2 moveInput;
+    void Start()
+    {
+        rigid=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        rigid.velocity = moveInput * speed;
+    }
+}
