62f04f3e6dfd57b4a7bd425b086ea25582bcd679
txh1005 <107378877+txh1005@users.noreply.github.com>
2023-12-03 13:32:07 +0700

    a
    a

--- Assets/Script/SetCursorImg.cs
+++ Assets/Script/SetCursorImg.cs
@@ -1,12 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SetCursorImg : MonoBehaviour
 {
-    public Texture2D cursor;
-    private void OnMouseDrag()
+    public Texture2D cursorSprite;
+    public Texture2D defaultCursor;
+    void Start()
     {
-        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+        // lưu Texture2D của ảnh ban đầu
+    }
+
+    void OnMouseDown()
+    {
+        Cursor.SetCursor(cursorSprite, Vector2.zero, CursorMode.Auto); // đổi cursor thành ảnh khi click chuột
+    }
+
+    void OnMouseUp()
+    {
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto); // đổi cursor về ảnh ban đầu khi thả chuột
     }
 }
--- Assets/Script/Sheep.cs
+++ Assets/Script/Sheep.cs
@@ -62,5 +62,4 @@ public class Sheep : MonoBehaviour
             }
         }
     }
-
 }
